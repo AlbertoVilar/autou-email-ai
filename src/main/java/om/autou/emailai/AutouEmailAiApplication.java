5eadac4a884a0284461e0c23b42f52d3a8167c14
Alberto Vilar <albertovilar1@gmail.com>
2026-01-17 13:35:20 -0300

    chore: bootstrap spring initializr project

---
+++ src/main/java/om/autou/emailai/AutouEmailAiApplication.java
@@ -0,0 +1,13 @@
+package om.autou.emailai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutouEmailAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutouEmailAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/om/autou/emailai/AutouEmailAiApplicationTests.java
@@ -0,0 +1,13 @@
+package om.autou.emailai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutouEmailAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
